6f5e5213bcced340cf7f739c00df7ba38d3554e5
nhatanh203 <vonhatanh462003@gmail.com>
2021-12-26 18:29:36 +0700

    upbaitap

--- 7_spring_data_repository/bai_tap/blog_application/src/main/java/com/codegym/blog_application/repository/ICategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.blog_application.repository;
-
-import com.codegym.blog_application.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository(value = "iCateRepo")
-public interface ICategoryRepository extends JpaRepository<Category,String> {
-}
